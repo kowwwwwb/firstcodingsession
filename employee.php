ea75983b440028a2aeb29f4604de01453fa92a06
kowwwwwb <201980020@psu.palawan.edu.ph>
2022-04-10 01:35:45 +0800

    pagination employees done

--- employee.php
+++ employee.php
@@ -23,8 +23,29 @@
     require('config/config.php');
     require('config/db.php');
 
+    // define total number of results you want per page
+    $results_per_page =25;
+
+    // find the total number of results/rows stored in the database
+    $query = "SELECT * FROM employee";
+    $result = mysqli_query($conn, $query);
+    $number_of_result = mysqli_num_rows($result);
+
+    // determine the total number of pages available
+    $number_of_page = ceil($number_of_result / $results_per_page);
+
+    // determine which page number visitor is currently on
+    if(!isset($GET['page'])){
+        $page = 1;
+    }else{
+        $page = $_GET['page'];
+    }
+
+    //determine the sql LIMIT starting  number for the results on the display page
+    $page_first_result = ($page-1) * $results_per_page;
+
     // Create query
-    $query = 'SELECT employee.lastname, employee.firstname, employee.address, office.name as office_name FROM employee, office WHERE employee.office_id = office.id ORDER BY employee.lastname';
+    $query = 'SELECT employee.lastname, employee.firstname, employee.address, office.name as office_name FROM employee, office WHERE employee.office_id = office.id ORDER BY employee.lastname LIMIT '. $page_first_result . ',' . $results_per_page;
 
     // Get the result
     $result = mysqli_query($conn, $query);
@@ -92,6 +113,12 @@
                             </div>
                         </div>
                 </div>
+                <?php
+                         for ($page=1; $page <= $number_of_page; $page++){
+                             echo '<a href ="employee.php?page='. $page . '">' . $page . '</a>';
+                         }                    
+                    
+                    ?>
                 </div>
             </div>
             <footer class="footer">
