32af31a531f879e89cb8f9c6dde21967926a6a67
kowwwwwb <201980020@psu.palawan.edu.ph>
2022-04-10 01:22:15 +0800

    pagination transaction

--- transaction.php
+++ transaction.php
@@ -23,9 +23,30 @@
     require('config/config.php');
     require('config/db.php');
 
+    // define total number of results you want per page
+    $results_per_page =10;
+
+    // find the total number of results/rows stored in the database
+    $query = "SELECT * FROM transaction";
+    $result = mysqli_query($conn, $query);
+    $number_of_result = mysqli_num_rows($result);
+
+    // determine the total number of pages available
+    $number_of_page = ceil($number_of_result / $results_per_page);
+
+    // determine which page number visitor is currently on
+    if(!isset($GET['page'])){
+        $page = 1;
+    }else{
+        $page = $_GET['page'];
+    }
+
+    //determine the sql LIMIT starting  number for the results on the display page
+    $page_first_result = ($page-1) * $results_per_page;
+
     // Create query
     $query = 'SELECT transaction.datelog, transaction.documentcode, transaction.action, transaction.remarks, office.name as office_name, CONCAT(employee.lastname, ",", employee.firstname) as employee_fullname FROM recordapp_db.employee, recordapp_db.office, recordapp_db.transaction 
-    WHERE transaction.employee_id=employee.id and transaction.office_id = office.id';
+    WHERE transaction.employee_id=employee.id and transaction.office_id = office.id LIMIT '. $page_first_result . ',' . $results_per_page;
 
     // Get the result
     $result = mysqli_query($conn, $query);
@@ -96,7 +117,13 @@
                                 </div>
                             </div>
                         </div>
-                </div>
+                    </div>
+                    <?php
+                         for ($page=1; $page <= $number_of_page; $page++){
+                             echo '<a href ="transaction.php?page='. $page . '">' . $page . '</a>';
+                         }                    
+                    
+                    ?>
                 </div>
             </div>
             <footer class="footer">
